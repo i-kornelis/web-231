9d148e7656d07ec33bac1cff502ef0e5a1396100
i-kornelis <izabellakornelis@gmail.com>
2019-11-29 19:56:19 -0800

    Adding assignment 1.3

---
+++ week-1/hello-world.js
@@ -0,0 +1,15 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Izabella Kornelis
+; Date: 29 November 2019
+; Modified By: Izabella Kornelis
+; Description: A simple JavaScript program to produce the the text "Hello World, My Name is Izabella Kornelis."
+;===========================================
+*/
+
+//start program
+
+console.log ("Hello World, My Name is Izabella Kornelis.")
+
+//end program
