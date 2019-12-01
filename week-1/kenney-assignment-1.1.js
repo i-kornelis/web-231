9e96f88a10f8a9ed2536405fb3363c4f22b3f409
i-kornelis <izabellakornelis@gmail.com>
2019-11-30 16:42:33 -0800

    Corrected kenney-assignment-1.1.js code

---
+++ week-1/kenney-assignment-1.1.js
@@ -0,0 +1,24 @@
+/*
+=================================================
+Title: Assignment 1.1
+Author: Sean Kenney
+Date: 27 November 2019
+Modified by: Izabella Kornelis
+Description: This program does not work.  It contains two errors that must be fixed.
+=================================================
+*/
+
+//Variables
+// variable that is assigned today's date
+var todaysDate = new Date();
+
+// variable that is assigned a past year
+var pastYear = 1776;
+
+//Output
+
+//Display the current date
+console.log("Today's date and time is " + todaysDate);
+
+//Display a past year value
+console.log(pastYear + " occurred in the past");
