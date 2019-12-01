96801e6f6409df24d2fb541d905a66a102d30077
i-kornelis <izabellakornelis@gmail.com>
2019-11-30 16:26:27 -0800

    Corrected code for 'giannone-exercise-1.1.js

---
+++ week-1/giannone-exercise-1.1.js
@@ -0,0 +1,18 @@
+/*
+=======================================
+Title: Assignment 1.1
+Author: Andrew Giannone
+Date: November 27 2019
+Modified By: Izabella Kornelis
+Description This program should use JavaScript to print the value of 2 numbers multiplied to the console.
+======================================
+*/
+
+//Variables
+var firstNumber = 23;
+var secondNumber = 2;
+var product = firstNumber * secondNumber;
+
+
+//Output
+console.log(product);
