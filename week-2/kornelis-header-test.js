e6b36e313eeb29c000c4bf460df41e042b660286
i-kornelis <izabellakornelis@gmail.com>
2019-12-04 19:45:36 -0800

    adding kornelis-header.js and test

---
+++ week-2/kornelis-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  kornelis-header-test.js
+; Author: Izabella Kornelis
+; Date:   04 December 2019
+; Modified By: Izabella Kornelis
+; Description: Test to displays a formatted header
+;===========================================
+*/
+
+const header = require('../kornelis-header.js')
+
+console.log(header.display("Izabella", "Kornelis", "Week 2"));
