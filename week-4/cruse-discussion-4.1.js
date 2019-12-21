19e198081f27e797231cb418f4dca9320e3b400e
i-kornelis <izabellakornelis@gmail.com>
2019-12-20 20:00:17 -0800

    fixed header issue in 4.1  and added cruse-discussion-4.1 corrections

---
+++ week-4/cruse-discussion-4.1.js
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title: Discussion 4.1
+; Author: Jess Cruse
+; Date: 20 Dec 2019
+; Modified By: Izabella Kornelis
+; Description: This program compares two number and provides error handling for if the numbers match or not.
+;===========================================
+*/
+
+// Require statement that imports the header.js file from my root directory.
+const header = require('../kornelis-header.js') // change to my header
+
+// Call the console.log() function and output a well-formatted header
+console.log(header.display("Izabella", "Kornelis", "Discussion 4.1"));
+
+// new line
+console.log("\n");
+
+// START PROGRAM
+
+var myNumbers = [1, 2, 3, 4, 5, 6];
+
+// output
+console.log("My Numbers are " + myNumbers);
+
+// END PROGRAM
