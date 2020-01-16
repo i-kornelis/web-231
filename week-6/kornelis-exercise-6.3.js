63b71f4b2e4b3358f510773056061f09bc6fcce5
i-kornelis <izabellakornelis@gmail.com>
2020-01-15 18:26:30 -0800

    Adding exercise 6.3

---
+++ week-6/kornelis-exercise-6.3.js
@@ -0,0 +1,31 @@
+/*
+============================================
+; Title: Assignment 6.3
+; Author: Izabella Kornelis
+; Date: 15 January 2020
+; Description: A program using get and set methods for objects.
+;===========================================
+*/
+
+// Require statement that imports the header.js file from my root directory.
+const header = require('../kornelis-header.js')
+
+// Call the console.log() function and output a well-formatted header
+console.log(header.display("Izabella", "Kornelis", "Assignment 6.3"));
+// new line
+console.log('\n')
+
+// start program
+
+// ticketing system object
+let ticketingSystem = {
+  id: "101",
+  name: "Help Desk Support",
+  requester: "Bob Jones"
+};
+
+// output to a single line
+console.log( "{id: " + ticketingSystem.id + ", name: " + ticketingSystem.name + ", requester: " + ticketingSystem.requester + "}")
+
+
+// end program
